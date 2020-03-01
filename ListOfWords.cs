1366b8a4910c5a50f26be02f352d54e72a15cf9b
lozal <lozovaalla@yandex.ru>
2020-03-01 21:40:57 +0300

    добавлен класс списка слов

---
+++ ListOfWords.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace LingvaDict
+{
+    public class ListOfWords
+    {
+        SortedDictionary<Word, int> words;
+        Menu menuSelectPartOfSpeech;
+        Menu menuSelectGender;
+        public ListOfWords()
+        {
+            words = new SortedDictionary<Word, int>();
+            menuSelectPartOfSpeech = CreateMenuPartOfSpeech();
+            menuSelectGender = CreateMenuSelectGender();
+        }
+
+        public void Add(Word word)
+        {
+            Write("Введите буквенное написание слова -->");
+            word.WriteLetter = ReadLine();
+            word.PartOfSpeech = (SetPartOfSpeech)menuSelectPartOfSpeech.SelectOption("Какая часть речи?");
+            if (word.PartOfSpeech != SetPartOfSpeech.Undefined)
+            {
+                switch (word.PartOfSpeech)
+                {
+                    case SetPartOfSpeech.Noun:
+                        word.GenderNoun = (SetGender)menuSelectGender.
+                            SelectOption("Выберите род существительного");
+                        Write("Введите форму множественного числа -->");
+                        word.PluralForm = ReadLine();
+                        Write("Введите смысловое описание слова -->");
+                        word.Description = ReadLine();
+                        break;
+                    case SetPartOfSpeech.Verb:
+                        WriteLine("глагол\n");
+                        break;
+                }
+            }
+            WriteLine("Вы ввели -->");
+            if (!words.ContainsKey(word))
+            {
+                words.Add(word, words.Count + 1);
+            }
+            foreach (Word w in words.Keys)
+            {
+                WriteLine("Запись {0}: {1} ", words[w], w);
+            }
+        }
+        static Menu CreateMenuPartOfSpeech()
+        {
+            Menu menu = new Menu(3);
+            menu[0] = new MenuOption("\tВозврат в предыдущее меню - цифра 0 -->");
+            menu[1] = new MenuOption("\t          Существительное - цифра 1");
+            menu[2] = new MenuOption("\t                   Глагол - цифра 2");
+            return menu;
+        }
+        static Menu CreateMenuSelectGender()
+        {
+            Menu menu = new Menu(4);
+            menu[0] = new MenuOption("\tВозврат в предыдущее меню - цифра 0 -->");
+            menu[1] = new MenuOption("\t                  Мужской - цифра 1");
+            menu[2] = new MenuOption("\t                  Женский - цифра 2");
+            menu[3] = new MenuOption("\t                  Средний - цифра 3");
+            return menu;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -20,8 +20,6 @@ namespace LingvaDict
             Menu menuModeOfUsing = CreateMenuModeOfUsing();
             Menu menuSelectLanguage = CreateMenuSelectLanguage();
             Menu menuSelectActWordsList = CreateMenuWordsList();
-            Menu menuSelectPartOfSpeech = CreateMenuPartOfSpeech();
-            Menu menuSelectGender = CreateMenuSelectGender();
             int modeOfUsing = 0;
             SetActWordsList actWordList = SetActWordsList.Undefined;
             do
@@ -34,7 +32,7 @@ namespace LingvaDict
                     {
                         case 1:
                             WriteLine("работа со списком слов\n");
-                            SortedDictionary<Word, int> words = new SortedDictionary<Word, int>();
+                            ListOfWords words = new ListOfWords();
                             do
                             {
                                 wordLingva = (SetLanguage)menuSelectLanguage.SelectOption("Выбор языка:");
@@ -77,39 +75,8 @@ namespace LingvaDict
                                                 case SetActWordsList.AddWord:
                                                     WriteLine("добавить запись \n");
                                                     Word word = new Word(wordLingva, userLingva, modeWrite);
-                                                    Write("Введите буквенное написание слова -->");
-                                                    word.WriteLetter = ReadLine();
-                                                    word.PartOfSpeech
-                                                        = (SetPartOfSpeech)menuSelectPartOfSpeech.
-                                                        SelectOption("Какая часть речи?");
-                                                    if (word.PartOfSpeech != SetPartOfSpeech.Undefined)
-                                                    {
-                                                        switch (word.PartOfSpeech)
-                                                        {
-                                                            case SetPartOfSpeech.Noun:
-                                                                word.GenderNoun = (SetGender)
-                                                                menuSelectGender.
-                                                                SelectOption("Выберите род существительного");
-                                                                Write("Введите форму множественного числа -->");
-                                                                word.PluralForm = ReadLine();
-                                                                Write("Введите смысловое описание слова -->");
-                                                                word.Description = ReadLine();
-                                                                break;
-                                                            case SetPartOfSpeech.Verb:
-                                                                WriteLine("глагол\n");
-                                                                break;
-                                                        }
-                                                    }
-                                                    WriteLine("Вы ввели -->");
-                                                    if (!words.ContainsKey(word))
-                                                    {
-                                                        words.Add(word, words.Count + 1);
-                                                    }
-                                                    foreach (Word w in words.Keys)
-                                                    {
-                                                        WriteLine("Запись {0}: {1} ", words[w], w);
-                                                    }
-                                                        break;
+                                                    words.Add(word);
+                                                    break;
                                                 case SetActWordsList.RemoveWord:
                                                     WriteLine("удалить запись \n");
                                                     break;
@@ -160,23 +127,6 @@ namespace LingvaDict
             menu[2] = new MenuOption("\t            Удалить слово - цифра 2");
             menu[3] = new MenuOption("\t      Редактировать слово - цифра 3");
             menu[4] = new MenuOption("\t           Показать слова - цифра 4");
-            return menu;
-        }
-        static Menu CreateMenuPartOfSpeech()
-        {
-            Menu menu = new Menu(3);
-            menu[0] = new MenuOption("\tВозврат в предыдущее меню - цифра 0 -->");
-            menu[1] = new MenuOption("\t          Существительное - цифра 1");
-            menu[2] = new MenuOption("\t                   Глагол - цифра 2");
-            return menu;
-        }
-        static Menu CreateMenuSelectGender()
-        {
-            Menu menu = new Menu(4);
-            menu[0] = new MenuOption("\tВозврат в предыдущее меню - цифра 0 -->");
-            menu[1] = new MenuOption("\t                  Мужской - цифра 1");
-            menu[2] = new MenuOption("\t                  Женский - цифра 2");
-            menu[3] = new MenuOption("\t                  Средний - цифра 3");
             return menu;
         }
     }
